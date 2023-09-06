d3a27d2c430c2d83b3939beed3ef3ece783af5dd
PawelBanasiewicz <pawel.j.banasiewicz@gmail.com>
2023-09-06 18:36:51 +0200

    put all files to main directory

---
+++ generate_games.py
@@ -0,0 +1,17 @@
+from constants import GENERATED_DATA_DATABASE_PATH, GAMES_OUTPUT_PATH
+from database_manager import DatabaseManager
+from games_data_generator import GameDataGenerator
+
+if __name__ == '__main__':
+    games_number = 1
+
+    games_data_generator = GameDataGenerator()
+    games_list = games_data_generator.generate_games_list(games_number)
+
+    database_manager = DatabaseManager([GENERATED_DATA_DATABASE_PATH])
+    database_manager.insert_games(GENERATED_DATA_DATABASE_PATH, games_list)
+    database_manager.generate_query_to_insert_games_to_mysql(GENERATED_DATA_DATABASE_PATH, GAMES_OUTPUT_PATH)
+
+    games_data_generator.close_connections()
+    database_manager.close()
+
---
+++ generate_players.py
@@ -0,0 +1,15 @@
+from constants import GENERATED_DATA_DATABASE_PATH, PLAYERS_OUTPUT_PATH
+from database_manager import DatabaseManager
+from player_data_generator import PlayerDataGenerator
+
+if __name__ == "__main__":
+    players_number = 50_000
+
+    PlayerDataGenerator = playerDataGenerator = PlayerDataGenerator()
+    players_list = playerDataGenerator.generate_players_list(players_number)
+
+    database_manager = DatabaseManager([GENERATED_DATA_DATABASE_PATH])
+    database_manager.insert_players(GENERATED_DATA_DATABASE_PATH, players_list)
+    database_manager.generate_query_to_insert_players_to_mysql(GENERATED_DATA_DATABASE_PATH, PLAYERS_OUTPUT_PATH)
+
+    database_manager.close()
